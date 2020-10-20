c268adfff80b7bd748a43f3ffe3c85e8e8d8faa5
MariaMalitckaya <maria.malitckaya@gmail.com>
2020-10-20 17:42:33 +0300

    remove buildozer

--- app/.buildozer/android/platform/build-armeabi-v7a/dists/kvcalc__armeabi-v7a/obj/local/armeabi-v7a/objs-debug/webpdecoder_static/src/utils/random_utils.o.d
+++
@@ -1,8 +0,0 @@
-/home/meeshkan-cain/side-project/python-app/app/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/webpdecoder_static/src/utils/random_utils.o: \
-  /home/meeshkan-cain/side-project/python-app/app/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp-1.0.0/src/utils/random_utils.c \
-  /home/meeshkan-cain/side-project/python-app/app/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp-1.0.0/src/utils/random_utils.h \
-  /home/meeshkan-cain/side-project/python-app/app/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp-1.0.0/src/webp/types.h
-
-/home/meeshkan-cain/side-project/python-app/app/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp-1.0.0/src/utils/random_utils.h:
-
-/home/meeshkan-cain/side-project/python-app/app/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp-1.0.0/src/webp/types.h:
